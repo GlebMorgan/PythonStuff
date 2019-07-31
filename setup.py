0c79bacc00f391ecc43a3b4390b75bff3b4fcfcb
GlebMorgan <glebmorgan@gmail.com>
2019-07-31 14:20:21 +0300

    Try to make a wheel package out of PyUtils Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='PyUtils',
+        version='0.43',
+        packages=['', 'PyQt5Utils'],
+        url='https://github.com/GlebMorgan/PythonStuff',
+        license='',
+        author='GlebMorgan',
+        author_email='glebmorgan@gmail.com',
+        description='Utilities to use cross-project'
+)
