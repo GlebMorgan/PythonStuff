0eaf754335f03afe3a67828eead2274df1c29a28
GlebMorgan <glebmorgan@gmail.com>
2019-09-18 17:04:50 +0300

    Colorer: redesigned .blink() to fade out gradually BlinkingValidator: fixed bugs in .validate() and .fixup() (invalid return values) Added .ownerBase attr to allow select different components for blinking and colorizing Created basic functionality to decorate functions/methods that needs to be blinked (not tested)

--- src/Experiments/colorer.py
+++ src/Experiments/colorer.py
@@ -1,6 +1,7 @@
 from __future__ import annotations as annotations_feature
 from enum import Enum
-from typing import Union
+from functools import wraps, partial, partialmethod
+from typing import Union, Dict, Callable
 
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QColor, QPalette, QValidator
@@ -9,14 +10,21 @@ from Utils import Logger
 
 log = Logger("Colorer")
 
-BACKGROUND_COLOR = 'white'
-BLINKING_DURATION = 100  # ms
 
+# TODO: choose colors for blinking
+
+# TODO: Replace TEMPvar with statement containing BLINKING_DURATION and BLUR_RADIUS (compute delay out of these two)
+
+
+BACKGROUND_COLOR = 'white'
+BLINKING_DURATION = 120  # ms
+BLUR_RADIUS = 15
+TEMP = 10  # TEMP
 
 class DisplayColor(Enum):
     Normal = 'black'
     Red = 'red'
-    Green = 'forestgreen'
+    Green = 'lime'  # TEMP 'forestgreen'
     Blue = 'mediumblue'
     Black = 'black'
     White = 'white'
@@ -31,29 +39,64 @@ class BlinkingValidator(QValidator):
 
     def validate(self, text, pos):
         state, text, pos = self.targetValidator.validate(text, pos)
-        if state == QValidator.Invalid: self.colorer.glow(DisplayColor.Red)
+        if state == QValidator.Invalid: self.colorer.blink(DisplayColor.Red)
+        return state, text, pos
 
     def fixup(self, text):
-        return self.targetValidator.fixup()
+        return self.targetValidator.fixup(text)
+
+
+class colored:  # TESTME
+    """ Decorator """
+    def __init__(self, colorerMethod: Callable, colorer: QWidget, colorsMapping: Dict[str, DisplayColor]):
+        self.colorer = colorer
+        self.colorerMethod: Callable = colorerMethod
+        self.colorsMapping: Dict[str, DisplayColor] = colorsMapping
+
+    def __call__(self, method: Callable):
+        @wraps(method)
+        def colorizedMethod(*args, **kwargs):
+            result = method(*args, **kwargs)
+            try: color = self.colorsMapping[result]
+            except KeyError: pass
+            else: self.colorerMethod(self.colorer, color)
+            return result
+
+        return colorizedMethod
 
 
 class Colorer():
-    def __init__(self, owner: QWidget):
-        self.owner: QWidget = owner
-        self.blinkingTimer: QTimer = None
+    """ ... TODO
+        Should be initialized after validator is set.
+            .patchValidator() should be called each time validator is changed
+    """
 
-    def setColor(self, role: QPalette.ColorRole, color: Union[QColor, str]):
+    def __init__(self, widget: QWidget, base: QWidget = None):
+        self.owner: QWidget = widget
+        self.ownerBase: QWidget = base if base is not None else widget
+
+        timer = QTimer(self.owner)
+        timer.setInterval(TEMP)
+        timer.timeout.connect(self.unblink)
+        self.blinkingTimer: QTimer = timer
+
+        QTimer().singleShot(0, self.patchValidator)
+
+    def setColor(self, role: QPalette.ColorRole, color: Union[DisplayColor, QColor, str]):
         """ Update 'owner' widget color component 'role' (text, selection, background, etc.)
                 with color 'color' """
-        palette = self.owner.palette()
+        if isinstance(color, DisplayColor): color = color.value
+        palette = self.ownerBase.palette()
         palette.setColor(role, QColor(color))
-        self.owner.setPalette(palette)
+        self.ownerBase.setPalette(palette)
 
-    def glow(self, color):
+    def glow(self, color: Union[DisplayColor, QColor, str]):
+        if isinstance(color, DisplayColor): color = color.value
+        # TODO: ▼ convert this to Chain() statement
         effect = QGraphicsDropShadowEffect()
         effect.setOffset(0)
-        effect.setBlurRadius(20)
-        effect.setColor(color)
+        effect.setBlurRadius(BLUR_RADIUS)
+        effect.setColor(QColor(color))
         self.owner.setGraphicsEffect(effect)
 
     def removeGlow(self):
@@ -62,33 +105,43 @@ class Colorer():
         QTimer().singleShot(BLINKING_DURATION // 4,
                             lambda: setattr(self, 'blinkingTimer', None) if not self.blinkingTimer.isActive() else None)
 
-    def blink(self, color: DisplayColor):
+    def blink_old(self, color: DisplayColor):
         """ Blink background with specified color for BLINKING_DURATION ms """
 
         if not self.blinkingTimer:
-            log.debug(f'▲ ({color.name})')
+            log.debug(f'☼ ({color.name})')
             self.blinkingTimer = QTimer(self.owner)
             self.blinkingTimer.setInterval(BLINKING_DURATION // 4 * 3)
             self.blinkingTimer.setSingleShot(True)
-            self.blinkingTimer.timeout.connect(lambda: self.removeGlow)
-            self.blinkingTimer.start()
+            self.blinkingTimer.timeout.connect(self.removeGlow)
         elif self.blinkingTimer.color != color:
             log.debug(f'↺ ({color.name})')
-            self.blinkingTimer.start()
-        else: return
-        log.debug('☼')
-        self.blinkingTimer.color = color
-        self.glow(color.name)
+        else:
+            log.debug('↓')
+            return
+        self.blinkingTimer.color: DisplayColor = color
+        self.blinkingTimer.start()
+        self.glow(color)
 
-    def _unblink_(self, oldColor: Union[DisplayColor, QColor]):
-        # log.debug(f"○ ({oldColor.name() if hasattr(oldColor.name, '__call__') else oldColor.name})")
-        self.setColor(QPalette.Text, oldColor)
-        self.setColor(QPalette.Base, DisplayColor.BackgroundNormal)
-        QTimer().singleShot(BLINKING_DURATION // 4,
-                            lambda: setattr(self, 'blinkingTimer', None) if not self.blinkingTimer.isActive() else None)
+    def blink(self, color: DisplayColor):
+        self.glow(color)
+        self.blinkingTimer.start()
+
+    def unblink(self):
+        effect = self.owner.graphicsEffect()
+        if effect.blurRadius() <= 1:
+            self.owner.setGraphicsEffect(None)
+            self.blinkingTimer.stop()
+        else:
+            effect.setBlurRadius(effect.blurRadius() - 1)
 
     def patchValidator(self):
         validator = self.owner.validator()
         log.debug(f"Owner validator is {validator}")
         if validator is not None:
-            self.owner.setValidator(BlinkingValidator(validator))
+            self.owner.setValidator(BlinkingValidator(self))
+
+    setBaseColor = partialmethod(setColor, QPalette.Base)
+
+    blinked = partial(colored, blink)  # decorator
+    colorized = partial(colored, setBaseColor)  # decorator
